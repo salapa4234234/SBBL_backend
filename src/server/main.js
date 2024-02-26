d0a63268b685e93d09aea4c2b861be1cd0f6dab4
nabarajrai <nabaraj2055@gmail.com>
2024-02-27 00:14:28 +0545

    register API done

--- src/server/main.js
+++ src/server/main.js
@@ -16,9 +16,77 @@ app.get("/api/employees", (req, res) => {
   });
 });
 
-app.post("/api/employees", (req, res) => {
-  // const query = `INSERT INTO employees()
+app.post("/api/register", async (req, res) => {
+  const {
+    firstName,
+    lastName,
+    address,
+    contact_number,
+    email,
+    designation,
+    department,
+    gender,
+  } = req.body;
+
+  const userWithEmail = await getByEmail(email);
+  if (userWithEmail) {
+    return res.status(400).json({ msg: "Email already exists" });
+  }
+  const userWithPhone = await getByPhone(contact_number);
+  if (userWithPhone) {
+    return res.status(400).json({ msg: "Phone already exists" });
+  }
+
+  const query = `INSERT INTO employees (firstName, lastName, address, contact_number, email, designation, department, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  const values = [
+    firstName,
+    lastName,
+    address,
+    contact_number,
+    email,
+    designation,
+    department,
+    gender,
+  ];
+  db.query(query, [...values], (err, data) => {
+    if (err) throw err;
+    return res
+      .status(200)
+      .json({ msg: "Successfully registered ", data: data });
+  });
 });
+function getByEmail(email) {
+  const query = `SELECT * FROM employees WHERE email =?`;
+  return new Promise((resolve, reject) => {
+    db.query(query, [email], (err, data) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      if (data.length > 0) {
+        resolve(data[0]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
+function getByPhone(phone) {
+  const query = `SELECT * FROM employees WHERE contact_number =?`;
+  return new Promise((resolve, reject) => {
+    db.query(query, [phone], (err, data) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      if (data.length > 0) {
+        resolve(data[0]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
 
 // Start the server
 ViteExpress.listen(app, 3000, () => {
@@ -38,7 +106,7 @@ function createTable() {
       email VARCHAR(255) NOT NULL,
       designation VARCHAR(255) NOT NULL,
       department VARCHAR(255) NOT NULL,
-      gender VARCHAR(255) NOT NULL
+      gender VARCHAR(1) NOT NULL
     )
   `;
 
