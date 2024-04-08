5fe7b9837a17cb130dee7d3d92a72e520f1de102
salapa <salapanabaraj2023@gmail.com>
2024-04-08 16:42:56 +0545

    setupd

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "papa2055",
-  database: "SBBL_DB",
+  host: "sql8.freesqldatabase.com",
+  user: "sql8697335",
+  password: "gHzMKWPAux",
+  database: "sql8697335",
 });
 
 db.connect((err) => {
