4f78829f1e92ce10aefe6c6938575a888eeee7a5
nabarajrai <nabaraj2055@gmail.com>
2024-04-08 08:01:31 +0545

    Revert "removed client"
    This reverts commit 2dc495629c9046d7393a87d1f59c9a77f825733a.

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: "sql8.freesqldatabase.com",
-  user: "sql8697335",
-  password: "gHzMKWPAux",
-  database: "sql8697335",
+  host: "127.0.0.1",
+  user: "root",
+  password: "papa2055",
+  database: "SBBL_DB",
 });
 
 db.connect((err) => {
