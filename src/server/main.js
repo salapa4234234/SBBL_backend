f5907c3e0091a928091a66b8725ae94acc4865c3
nabarajrai <nabaraj2055@gmail.com>
2024-04-10 09:03:50 +0545

    set header

--- src/server/main.js
+++ src/server/main.js
@@ -27,6 +27,18 @@ function verifyToken(req, res, next) {
     }
     req.user = decoded;
     res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://phonebook.salapabikasbank.com.np"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
     next();
   });
 }
